f757dbdc5776e0131b4c4198e75e89b0ef562fe8
seetler <jsnzeng@gmail.com>
2025-01-12 12:04:41 -0800

    file struct

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-import flask
+from flask import Flask
 from algo import *
 from rob import *
 
+app =  Flask(__name__)
 
+@app.route("/")
+def hello_world():
+    return "xd sl"
 
+app.run(host="0.0.0.0", port=5067)
